69f1c1c03cfeda82511979dcb9e7dd973e92e5eb
lawanimahi-star <lawanimahi@gmail.com>
2026-03-13 07:37:38 +0000

    solutions

---
+++ Conditions/solution1.py
@@ -0,0 +1,9 @@
+age = int(input("Enter age:"))
+if age < 13:
+    print("child")
+elif age <= 19:
+    print("Teenager")
+elif age <=59:
+    print("Adult")
+else: 
+    print("Senior") 
---
+++ Conditions/solution5.py
@@ -0,0 +1,9 @@
+weather = str(input("Enter weather:"))
+if weather == "sunny":
+    print("go for awalk")
+elif weather == "Rainy":
+    print("Read a book")
+elif weather == "snowy":
+    print("build a snow man")
+else:
+    print("Enter correct weather")
